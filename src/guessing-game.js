83c9434b2e8f4adb211ee6f4db85772858a30102
desen <32864798+desen94@users.noreply.github.com>
2021-09-06 23:31:20 +0500

    feat: implement task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+		constructor() {}
+		
+		setRange(min, max) {
+				this.min = min
+				this.max = max
+		}
+		
+		guess() {
+				this.min + Math.ceil((this.max - this.min) / 2)
+		}
+		
+		lower() {
+				this.max = this.guess()
+		}
+		
+		greater() {
+				this.min = this.guess()
+		}
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame
